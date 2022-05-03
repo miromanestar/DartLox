a22652ac8414f639383c74e77f1e7df851e7460a
Miro Manestar <miroimanestar@gmail.com>
2022-05-03 12:03:11 -0400

    Started adding tests

--- test/cli_test.dart
+++ test/cli_test.dart
@@ -1,8 +1,18 @@
-import 'package:cli/cli.dart';
 import 'package:test/test.dart';
+import 'dart:io';
+
+// void main() {
+//   test('calculate', () {
+//     expect(calculate(), 42);
+//   });
+// }
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+
+  test('bruh', () {
+      var files = Directory('test/files').listSync();
+      print(files);
+      
+      expect(true, true);
   });
 }
